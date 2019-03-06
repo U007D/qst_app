62d1d5f2012cfe6f3789cda0a8b884f231cfa70f
Brad Gibson <b2b@humanenginuity.com>
2019-03-06 08:45:54 -0800

    removed lib

--- src/args.rs
+++ src/args.rs
@@ -6,7 +6,7 @@ mod unit_tests;
 /// Args is a structure representing the user's supplied command-line arguments supplied to the program as an
 /// initialized data structure.  Types within the data structure are defined to be as restrictive as possible to
 /// ensure only valid inputs are accepted.  Where types accurately reflect the application's constraints, conditional
-/// logic, validation code & tests can all be safely elminiated, ∵ the invalid states of concern are not representable.
+/// logic, validation code & tests can all be safely eliminated, ∵ the invalid states of concern are not representable.
 #[derive(Debug, StructOpt)]
 #[structopt(rename_all = "kebab-case")]
 pub struct Args {
