dbde3d97c74bf481087493347459df877db98f58
bRad <brad@humanenginuity.com>
2019-04-05 09:41:22 -0700

    added warnings for rust_2018_idioms, pretty_assertions dev dependency and missing README + MIT, APACHE license files, improved tests for sample arg

--- src/args.rs
+++ src/args.rs
@@ -3,13 +3,10 @@ use structopt::StructOpt;
 #[cfg(test)]
 mod unit_tests;
 
-/// Args is a structure representing the user's supplied command-line arguments supplied to the program as an
-/// initialized data structure.  Types within the data structure are defined to be as restrictive as possible to
-/// ensure only valid inputs are accepted.  Where types accurately reflect the application's constraints, conditional
-/// logic, validation code & tests can all be safely eliminated, ∵ the invalid states of concern are not representable.
+/// Args is a data structure representing the user's supplied command-line arguments supplied to the program.
 #[derive(Debug, StructOpt)]
 #[structopt(rename_all = "kebab-case")]
 pub struct Args {
-    //    /// Sample argument (can be of any type).  Use `Option<some_type>` for optional arguments
-//    pub some_arg: usize,
+//    /// Optional argument indicating absence or presence and amount of `some_feature`.
+//    pub some_arg: Option<usize>,
 }
