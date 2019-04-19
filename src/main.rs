c951b3aec2f30465a0b02a5a78a4d590ae8a8eaa
Brad Gibson <brad@humanenginuity.com>
2019-04-19 06:13:48 -0700

    tweaked warnings

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,17 @@
 #![warn(clippy::all, clippy::nursery, clippy::pedantic, rust_2018_idioms)]
-// To use `unsafe` keyword, change to `#![allow(unsafe_code)]` (do not remove); aids auditing.
-#![forbid(bare_trait_objects, unsafe_code)]
-#![forbid(overflowing_literals)]
-// Safety-critical application lints
-#![deny(
-    clippy::pedantic,
-    clippy::float_cmp_const,
-    clippy::indexing_slicing,
-    clippy::integer_arithmetic,
-    clippy::option_unwrap_used,
-    clippy::result_unwrap_used
-)]
-// Uncomment before ship to reconcile use of possibly redundant crates, debug remnants, missing license file, and more
-//#![warn(clippy::restriction, missing_docs, warnings)]
-//#![deny(warnings)]
+#![forbid(bare_trait_objects)]
 #![allow(clippy::match_bool)]
+
+// To use the `unsafe` keyword, change to `#![allow(unsafe_code)]` (do not remove); aids auditing.
+#![forbid(unsafe_code)]
+
+// Safety-critical application lints
+#![deny(clippy::pedantic, clippy::float_cmp_const, clippy::indexing_slicing, clippy::integer_arithmetic,
+clippy::option_unwrap_used, clippy::result_unwrap_use)]
+
+// Uncomment before ship to reconcile use of possibly redundant crates, debug remnants, missing license files and more
+//#![warn(clippy::cargo, clippy::restriction, missing_docs, warnings)]
+//#![deny(warnings)]
 
 mod args;
 mod consts;
@@ -24,7 +21,7 @@ mod foo;
 use std::result::Result as StdResult;
 use structopt::StructOpt;
 pub use {args::Args, consts::*, error::Error};
-pub use foo::foo;
+pub use foo::Foo;
 
 pub type Result<T> = StdResult<T, Error>;
 
