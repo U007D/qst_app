fce0336c31fcb033d139aaae28e670720ac11653
u007d <b2b@humanenginuity.com>
2020-04-30 05:58:02 -0700

    switched back to `structopt`; updated lints, rustfmt.toml

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,7 @@
-#![warn(clippy::all, clippy::nursery, clippy::pedantic, rust_2018_idioms)]
-#![forbid(bare_trait_objects)]
-#![allow(clippy::match_bool)]
 // To use the `unsafe` keyword, change to `#![allow(unsafe_code)]` (do not remove); aids auditing.
 #![forbid(unsafe_code)]
+#![forbid(bare_trait_objects)]
+#![warn(clippy::all, clippy::nursery, clippy::pedantic, rust_2018_idioms)]
 // Safety-critical application lints
 #![deny(
     clippy::pedantic,
@@ -12,22 +11,27 @@
     clippy::option_unwrap_used,
     clippy::result_unwrap_used
 )]
-
-// Uncomment before ship to reconcile use of possibly redundant crates, debug remnants, missing license files and more
+#![allow(
+    clippy::match_bool,
+    clippy::iter_nth_zero,
+    clippy::module_name_repetitions
+)]
+// Uncomment before ship to reconcile use of possibly redundant crates, debug remnants, missing
+// license files and more
 //#![warn(clippy::cargo, clippy::restriction, missing_docs, warnings)]
 //#![deny(warnings)]
 
+mod args;
 mod consts;
 mod error;
 
 use pico_args::Arguments;
-pub use {consts::*, error::Error};
+pub use {args::Args, consts::*, error::Error};
 
 pub type Result<T, E = Error> = std::result::Result<T, E>;
 
 fn main() -> Result<()> {
-    // see examples/pico_args.rs for argument parsing example
-    let _args_parser = Arguments::from_env();
+    let _args = Args::from_args();
 
     Ok(())
 }
